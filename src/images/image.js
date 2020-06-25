839ff40c13834d1eed6de85f5353ee4f7891e38b
Khomenko Vlad <zatmenie8@gmail.com>
2020-06-25 13:43:23 +0300

    add images

---
+++ src/images/image.js
@@ -0,0 +1,108 @@
+import headerLogo from './home/logo.jpg';
+const headerImages = {
+  headerLogo
+}
+
+import homeBackgroundPicture from './home/picture.jpg';
+import homeKolrit from './home/Закарпатський колорит.png';
+import homeKoliba from './home/Колиба.jpg';
+import homePodvirja from './home/Сільське подвір’я.jpg';
+import homeHarchev from './home/Харчевня.jpg';
+import homeIcon1 from './home/icon1.png';
+import homeIcon2 from './home/icon2.png';
+import homeIcon3 from './home/icon3.png';
+import homeIcon4 from './home/icon4.png';
+const homeImages = {
+  homeBackgroundPicture,
+  homeKolrit,
+  homeKoliba,
+  homePodvirja,
+  homeHarchev,
+  homeIcon1,
+  homeIcon2,
+  homeIcon3,
+  homeIcon4
+}
+
+import koloritLogo from './kolorit/logo.jpg';
+import koloritAbout1 from './kolorit/rest-1.jpg';
+import koloritAbout2 from './kolorit/rest-2.jpg';
+import koloritAbout3 from './kolorit/rest-3.jpg';
+import koloritAbout4 from './kolorit/rest-4.jpg';
+const koloritImages = {
+  koloritLogo,
+  koloritAbout1,
+  koloritAbout2,
+  koloritAbout3,
+  koloritAbout4
+}
+
+import kolibaLogo from './koliba/logo.jpg';
+import kolibaOutside from './koliba/koliba-outside.jpg';
+import kolibaOutsideMix from './koliba/outside.jpg';
+import kolibaInterior1 from './koliba/interior1.jpg';
+import kolibaInterior2 from './koliba/interior2.jpg';
+import kolibaInterior3 from './koliba/interior3.jpg';
+import kolibaWaiters from './koliba/waiters1.jpg';
+import kolibaBread1 from './koliba/bread1.jpg';
+import kolibaBread2 from './koliba/bread2.jpg';
+import kolibaBread3 from './koliba/bread3.jpg';
+const kolibaImages = {
+  kolibaLogo,
+  kolibaOutside,
+  kolibaOutsideMix,
+  kolibaInterior1,
+  kolibaInterior2,
+  kolibaInterior3,
+  kolibaWaiters,
+  kolibaBread1,
+  kolibaBread2,
+  kolibaBread3
+}
+
+import podvirjaLogo from './podvirja/logo.jpg';
+import podvirjaSchema from './podvirja/schema.jpg';
+import podvirjaSauna from './podvirja/сауна.jpg';
+import podvirjaBanja from './podvirja/баня.jpg';
+import podvirjaMassage from './podvirja/масаж.jpg';
+import podvirjaBoat from './podvirja/катер.jpg';
+import podvirjaHorses from './podvirja/кони.jpg';
+import podvirjaPlace from './podvirja/площадка.jpg';
+import podvirjaHive1 from './podvirja/улик1.jpg';
+import podvirjaHive2 from './podvirja/улик2.jpg';
+import podvirjaRestorant from './podvirja/ресторан.jpg';
+const podvirjaImages = {
+  podvirjaLogo,
+  podvirjaSchema,
+  podvirjaSauna,
+  podvirjaBanja,
+  podvirjaMassage,
+  podvirjaBoat,
+  podvirjaHorses,
+  podvirjaPlace,
+  podvirjaHive1,
+  podvirjaHive2,
+  podvirjaRestorant
+}
+
+import harchevLogo from './harchev/logo.jpg';
+import harchevLogoAbout from './harchev/harchev-logo.jpg';
+import harchevByk from './harchev/бычок.jpg';
+import harchevHorses from './harchev/лошади.jpg';
+import harchevHall from './harchev/hall.jpg';
+const harchevImages = {
+  harchevLogo,
+  harchevLogoAbout,
+  harchevByk,
+  harchevHorses,
+  harchevHall
+}
+
+export {
+  headerImages,
+  homeImages,
+  koloritImages,
+  kolibaImages,
+  podvirjaImages,
+  harchevImages
+}
